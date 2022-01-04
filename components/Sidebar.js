73e52b33ad44c90a375191479e8f08ef5262055b
Alexnorvag <norvag@mail.ru>
2022-01-04 17:37:33 +0300

    added: login screen and auth middleware

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,3 +1,4 @@
+import { signOut, useSession } from "next-auth/react";
 import {
   HeartIcon,
   HomeIcon,
@@ -8,10 +9,20 @@ import {
 } from "@heroicons/react/outline";
 
 function Sidebar() {
+  const { data: session, status } = useSession();
+
+  console.log('session: ', session);
+
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
       <div className="space-y-4">
-        <button className="flex items-center space-x-2 hover:text-white ">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={signOut}
+        >
+          <p>Log out</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
         </button>
--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,26 @@
-import { getProviders } from "next-auth/react";
+import Image from "next/image";
+import { getProviders, signIn } from "next-auth/react";
+
+import logo from "../public/logo.png";
 
 function Login({ providers }) {
-
-  console.log('providers: ', providers);
+  console.log("providers: ", providers);
   return (
-    <div>
-      <h1>This is login page</h1>
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      <div className="w-52 mb-5">
+        <Image src={logo} alt="Logo" objectFit="cover" />
+      </div>
+
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="bg-[#18D860] text-white p-5 rounded-full"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
