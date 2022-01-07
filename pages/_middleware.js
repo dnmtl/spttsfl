79057ad6b52ed1d2a2b511ea08cf7133a8f1da75
Alexnorvag <norvag@mail.ru>
2022-01-07 04:22:37 +0300

    fixed: vercel deploy build

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,7 +4,13 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   console.log("MIDDLEWARE -> process.env.JWT_SECRET: ", process.env.JWT_SECRET);
   console.log("MIDDLEWARE -> req: ", req);
-  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+  const token = await getToken({
+    req,
+    secret: process.env.JWT_SECRET,
+    secureCookie:
+      process.env.NEXTAUTH_URL?.startsWith("https://") ??
+      !!process.env.VERCEL_URL,
+  });
 
   const { pathname } = req.nextUrl;
   console.log("MIDDLEWARE -> pathname: ", pathname);
