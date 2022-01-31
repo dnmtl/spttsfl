5864445786cead09b0e8ce9e6f0aef1a670f03d0
Alexnorvag <norvag@mail.ru>
2022-01-31 05:39:23 +0300

    change: redesigned profile page

--- pages/me.js
+++ pages/me.js
@@ -17,56 +17,54 @@ const MePage = () => {
   }
 
   return (
-    <>
+    <div className="w-full h-full md:flex md:items-center md:flex-col md:justify-evenly overflow-auto">
       <Head>
         <title>{profile.display_name} Profile - Spotify</title>
       </Head>
 
-      <div className="relative w-auto md:w-1/6 h-full md:h-auto md:m-auto md:aspect-square md:absolute md:top-1/3 md:-translate-y-1/2 md:left-1/2 md:-translate-x-1/2 object-cover opacity-70">
+      <Link href={"/"}>
+        <div className="absolute top-5 left-5 button-glassy rounded-full p-2">
+          <ArrowLeftIcon className="w-5 h-5" />
+        </div>
+      </Link>
+
+      <div className="relative w-full md:w-auto h-full md:h-full md:max-h-56 lg:max-h-80 md:aspect-square">
         <Image
           className="md:rounded-full"
           src={profile.images[0].url}
           layout="fill"
           objectFit="cover"
           alt="me"
+          quality={100}
         />
       </div>
+      <div className="absolute md:relative inset-x-0 bottom-0 bg-gradient-to-b from-transparent to-black md:bg-none px-4">
+        <div className="flex md:justify-center items-center space-x-2 text-glassy-60">
+          <LocationMarkerIcon className="w-5 h-5" />
+          <div className="font-thin">{profile.country}</div>
+        </div>
 
-      <div className="absolute inset-0">
-        <Link href={"/"}>
-          <div className="absolute top-5 left-5 button-glassy rounded-full p-2">
-            <ArrowLeftIcon className="w-5 h-5" />
+        <div className="flex md:justify-center items-center space-x-2">
+          <div className="text-4xl md:text-7xl font-medium uppercase my-3">
+            {profile.display_name}
           </div>
-        </Link>
+          {profile.isPremium && (
+            <BadgeCheckIcon className="w-5 h-5 text-spotify" />
+          )}
+        </div>
 
-        <div className="absolute inset-0 top-3/4 sm:top-2/3 bg-gradient-to-b from-transparent to-black px-4">
-          <div className="flex md:justify-center items-center space-x-2 text-glassy-60">
-            <LocationMarkerIcon className="w-5 h-5" />
-            <div className="font-thin">{profile.country}</div>
-          </div>
-
-          <div className="flex md:justify-center items-center space-x-2">
-            <div className="text-4xl md:text-7xl font-medium uppercase my-3">
-              {profile.display_name}
-            </div>
-            {profile.isPremium && (
-              <BadgeCheckIcon className="w-5 h-5 text-spotify" />
-            )}
-          </div>
-
-          <div className="flex md:justify-center space-x-4">
-            <div className="flex flex-col">
-              <span className="text-glassy-90 text-xl md:text-center">
-                {profile.followers.total}
-              </span>
-              <span className="text-glassy-50 capitalize text-sm font-medium">
-                Followers
-              </span>
-            </div>
+        <div className="flex md:justify-center space-x-4">
+          <div className="flex flex-col">
+            <span className="text-glassy-90 text-xl md:text-center">
+              {profile.followers.total}
+            </span>
+            <span className="text-glassy-50 capitalize text-sm font-medium">
+              Followers
+            </span>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
